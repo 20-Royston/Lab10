416f2a314d55bd965699f33d6f553f4101468790
20-Royston <coldroy1235@gmail.com>
2022-10-31 15:51:45 +0800

    Summary

---
+++ Lab10 - Project Starter/Assets/Tests/TestSuite.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestSuite
+    {
+        private float result = 0.0f;
+
+        [Test]
+        public void TestAdditional()
+        {
+            result = Calculator.CalculatePair(5, 2, "+");
+            Assert.AreEqual(result, 7);
+        }
+
+
+        [UnityTest]
+        public IEnumerator TestUnityAddition()
+        {
+            yield return null;
+
+            result = Calculator.CalculatePair(5, 2, "+");
+            Assert.AreEqual(result, 7);
+
+        }
+    }
+}
